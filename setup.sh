69c45f5c7ae34f8e13f82b7c6202d19ab9303809
Robby Bennett <robby@scitools.com>
2021-12-16 00:06:17 -0700

    Dash to panel

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 # Todo:
-# Applications in top bar, not grouped, monochrome
+# Show Applications tweaks
+# Keyboard shortcuts
 
 
 # Git
@@ -56,12 +57,75 @@ gsettings set $setting cursor-shape 'ibeam'
 gsettings set $setting scrollbar-policy 'never'
 
 # Extensions
-# Enable Desktop Icons
-gnome-extensions enable ding@rasterssoft.com
+# Enable Desktop Icons NG (DING)
+gnome-extensions enable ding@rastersoft.com
 gsettings set org.gnome.shell.extensions.ding icon-size 'small'
 gsettings set org.gnome.shell.extensions.ding show-drop-place false
 gsettings set org.gnome.shell.extensions.ding show-home false
 gsettings set org.gnome.shell.extensions.ding show-trash false
+# Disable Dash to Dock
+gnome-extensions disable dash-to-dock@micxgx.gmail.com
+# Enable Dash to Panel
+gnome-extensions enable dash-to-panel@jderose9.github.com
+gsettings set org.gnome.shell.extensions.dash-to-panel animate-app-switch true
+gsettings set org.gnome.shell.extensions.dash-to-panel animate-appicon-hover false
+gsettings set org.gnome.shell.extensions.dash-to-panel animate-appicon-hover-animation-extent '{"RIPPLE": 4, "PLANK": 4, "SIMPLE": 1}'
+gsettings set org.gnome.shell.extensions.dash-to-panel appicon-margin 4
+gsettings set org.gnome.shell.extensions.dash-to-panel appicon-padding 4
+gsettings set org.gnome.shell.extensions.dash-to-panel click-action 'CYCLE-MIN'
+gsettings set org.gnome.shell.extensions.dash-to-panel dot-position 'TOP'
+gsettings set org.gnome.shell.extensions.dash-to-panel dot-size 0
+gsettings set org.gnome.shell.extensions.dash-to-panel dot-style-focused 'DOTS'
+gsettings set org.gnome.shell.extensions.dash-to-panel dot-style-unfocused 'DOTS'
+gsettings set org.gnome.shell.extensions.dash-to-panel focus-highlight false
+gsettings set org.gnome.shell.extensions.dash-to-panel group-apps=false
+gsettings set org.gnome.shell.extensions.dash-to-panel group-apps-label-font-size=14
+gsettings set org.gnome.shell.extensions.dash-to-panel group-apps-label-font-weight 'inherit'
+gsettings set org.gnome.shell.extensions.dash-to-panel group-apps-label-max-width 0
+gsettings set org.gnome.shell.extensions.dash-to-panel group-apps-underline-unfocused true
+gsettings set org.gnome.shell.extensions.dash-to-panel hide-overview-on-startup false
+gsettings set org.gnome.shell.extensions.dash-to-panel hotkeys-overlay-combo 'TEMPORARILY'
+gsettings set org.gnome.shell.extensions.dash-to-panel intellihide false
+gsettings set org.gnome.shell.extensions.dash-to-panel intellihide-hide-from-windows false
+gsettings set org.gnome.shell.extensions.dash-to-panel isolate-monitors false
+gsettings set org.gnome.shell.extensions.dash-to-panel isolate-workspaces false
+gsettings set org.gnome.shell.extensions.dash-to-panel leftbox-padding -1
+gsettings set org.gnome.shell.extensions.dash-to-panel leftbox-size 0
+gsettings set org.gnome.shell.extensions.dash-to-panel middle-click-action 'LAUNCH'
+gsettings set org.gnome.shell.extensions.dash-to-panel overview-click-to-exit false
+gsettings set org.gnome.shell.extensions.dash-to-panel panel-anchors '{"0":"MIDDLE"}'
+gsettings set org.gnome.shell.extensions.dash-to-panel panel-element-positions '{"0":[{"element":"showAppsButton","visible":true,"position":"stackedTL"},{"element":"dateMenu","visible":true,"position":"stackedTL"},{"element":"activitiesButton","visible":false,"position":"stackedTL"},{"element":"leftBox","visible":false,"position":"stackedTL"},{"element":"taskbar","visible":true,"position":"centerMonitor"},{"element":"centerBox","visible":false,"position":"stackedBR"},{"element":"rightBox","visible":true,"position":"stackedBR"},{"element":"systemMenu","visible":true,"position":"stackedBR"},{"element":"desktopButton","visible":true,"position":"stackedBR"}]}'
+gsettings set org.gnome.shell.extensions.dash-to-panel panel-lengths '{"0":100}'
+gsettings set org.gnome.shell.extensions.dash-to-panel panel-positions '{"0":"TOP"}'
+gsettings set org.gnome.shell.extensions.dash-to-panel panel-sizes '{"0":48}'
+gsettings set org.gnome.shell.extensions.dash-to-panel peek-mode true
+gsettings set org.gnome.shell.extensions.dash-to-panel preview-middle-click-close true
+gsettings set org.gnome.shell.extensions.dash-to-panel preview-use-custom-opacity false
+gsettings set org.gnome.shell.extensions.dash-to-panel scroll-icon-action 'NOTHING'
+gsettings set org.gnome.shell.extensions.dash-to-panel scroll-panel-action 'NOTHING'
+gsettings set org.gnome.shell.extensions.dash-to-panel secondarymenu-contains-showdetails true
+gsettings set org.gnome.shell.extensions.dash-to-panel shift-click-action 'MINIMIZE'
+gsettings set org.gnome.shell.extensions.dash-to-panel shift-middle-click-action='LAUNCH'
+gsettings set org.gnome.shell.extensions.dash-to-panel show-appmenu false
+gsettings set org.gnome.shell.extensions.dash-to-panel show-favorites false
+gsettings set org.gnome.shell.extensions.dash-to-panel show-favorites-all-monitors true
+gsettings set org.gnome.shell.extensions.dash-to-panel show-tooltip true
+gsettings set org.gnome.shell.extensions.dash-to-panel show-window-previews true
+gsettings set org.gnome.shell.extensions.dash-to-panel status-icon-padding 8
+gsettings set org.gnome.shell.extensions.dash-to-panel stockgs-force-hotcorner false
+gsettings set org.gnome.shell.extensions.dash-to-panel stockgs-keep-dash false
+gsettings set org.gnome.shell.extensions.dash-to-panel stockgs-keep-top-panel false
+gsettings set org.gnome.shell.extensions.dash-to-panel stockgs-panelbtn-click-only false
+gsettings set org.gnome.shell.extensions.dash-to-panel trans-use-custom-bg false
+gsettings set org.gnome.shell.extensions.dash-to-panel trans-use-custom-gradient false
+gsettings set org.gnome.shell.extensions.dash-to-panel tray-padding 2
+gsettings set org.gnome.shell.extensions.dash-to-panel tray-size 0
+gsettings set org.gnome.shell.extensions.dash-to-panel window-preview-fixed-x false
+gsettings set org.gnome.shell.extensions.dash-to-panel window-preview-fixed-y true
+gsettings set org.gnome.shell.extensions.dash-to-panel window-preview-padding 8
+gsettings set org.gnome.shell.extensions.dash-to-panel window-preview-show-title false
+gsettings set org.gnome.shell.extensions.dash-to-panel window-preview-size 180
+gsettings set org.gnome.shell.extensions.dash-to-panel window-preview-title-position 'TOP'
 
 # Uninstallation
 # User Apps
